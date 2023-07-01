7bb85e213756933ea27761afcb3f7449d4deeb6a
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-07-01 13:43:29 +0200

    table update

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { axiosHandler } from "../../api/axios";
-import countVotes from "../../hooks/countVotes";
 import PropTypes from "prop-types";
 
 //default image
@@ -10,26 +9,13 @@ import default_image from "../../assets/default.png";
 //Styles
 import "./table.css";
 
-//Icons
-import arrow_up from "../../assets/icons/arrow-up.svg";
-import arrow_down from "../../assets/icons/arrow-down.svg";
-import up_vote from "../../assets/icons/up_vote.svg";
-import down_vote from "../../assets/icons/down_vote.svg";
-import filter from "../../assets/icons/filter.svg";
-import checkbox from "../../assets/icons/checkbox.svg";
-import empty_checkbox from "../../assets/icons/empty-checkbox.svg";
-import close from "../../assets/icons/close.svg";
 import { Box, Card } from "@mui/material";
+import { MaterialReactTable } from "material-react-table";
 
 const Table = ({ reload, setReload }) => {
   const navigate = useNavigate();
   const [data, setData] = React.useState([]);
-
-  const [sort, setSort] = React.useState("name_asc");
-  const [filteredPlatforms, setFilteredPlatforms] = React.useState([]);
-  const [showFilter, setShowFilter] = React.useState(false);
   const [filteredData, setFilteredData] = React.useState(data);
-  const [selectedPage, setSelectedPage] = React.useState(1);
   const [platforms, setPlatforms] = React.useState([]);
 
   React.useEffect(() => {
@@ -82,252 +68,87 @@ const Table = ({ reload, setReload }) => {
     };
   }, [reload]);
 
-  const changeFilter = (filter) => {
-    setSelectedPage(1);
-    if (filter === "name") {
-      if (!sort.includes(filter)) {
-        const sortedData = filteredData.sort((a, b) => {
-          let fa = a.name.toLowerCase(),
-            fb = b.name.toLowerCase();
-
-          if (fa < fb) {
-            return -1;
-          }
-          if (fa > fb) {
-            return 1;
-          }
-          return 0;
-        });
-        setSort(`${filter}_asc`);
-        setFilteredData(sortedData);
-      } else if (sort.includes(`${filter}_asc`)) {
-        const sortedData = filteredData.sort((a, b) => {
-          let fa = a.name.toLowerCase(),
-            fb = b.name.toLowerCase();
-
-          if (fa < fb) {
-            return 1;
-          }
-          if (fa > fb) {
-            return -1;
-          }
-          return 0;
-        });
-        setSort(`${filter}_desc`);
-        setFilteredData(sortedData);
-      } else {
-        const sortedData = filteredData.sort((a, b) => {
-          let fa = a.name.toLowerCase(),
-            fb = b.name.toLowerCase();
-
-          if (fa < fb) {
-            return -1;
-          }
-          if (fa > fb) {
-            return 1;
-          }
-          return 0;
-        });
-        setSort(`${filter}_asc`);
-        setFilteredData(sortedData);
-      }
-    } else {
-      if (!sort.includes(filter)) {
-        const sortedData = filteredData.sort(
-          (a, b) =>
-            b.votes_up / (b.votes_up + b.votes_down) -
-            a.votes_up / (a.votes_up + a.votes_down)
-        );
-        setSort(`${filter}_asc`);
-        setFilteredData(sortedData);
-      } else if (sort.includes(`${filter}_asc`)) {
-        const sortedData = filteredData.sort(
-          (a, b) =>
-            a.votes_up / (a.votes_up + a.votes_down) -
-            b.votes_up / (b.votes_up + b.votes_down)
-        );
-        setSort(`${filter}_desc`);
-        setFilteredData(sortedData);
-      } else {
-        const sortedData = filteredData.sort(
-          (a, b) =>
-            b.votes_up / (b.votes_up + b.votes_down) -
-            a.votes_up / (a.votes_up + a.votes_down)
-        );
-        setSort(`${filter}_asc`);
-        setFilteredData(sortedData);
-      }
-    }
-  };
-
-  function setImage(platform) {
-    const platformName = platform.toLowerCase();
-
-    const platformImage = platforms.find(
-      (platform) => platform.name.toLowerCase() === platformName
-    );
-
-    if (!platformImage) {
+  const setPlatformImage = (platform) => {
+    const selectPaltform = platforms.find((item) => item.name === platform);
+    if (!selectPaltform) {
       return null;
     }
-
-    return `${process.env.REACT_APP_SERVER_URL}/platforms/${platformImage.image}`;
-  }
-
-  const handleSearch = (e) => {
-    const search = e.target.value.toLowerCase();
-    const filteredData = data.filter((item) => {
-      return item.name.toLowerCase().includes(search);
-    });
-    setFilteredData(filteredData);
+    return `${process.env.REACT_APP_SERVER_URL}/platforms/${selectPaltform.image}`;
   };
 
-  const renderPagination = () => {
-    const pages = Math.ceil(filteredData.length / 10);
-    function selectedStyle(number) {
-      return {
-        backgroundColor: selectedPage === number ? "#0ef6cc" : undefined,
-        color: selectedPage === number ? "#000" : undefined,
-      };
-    }
-    if (selectedPage > 3 && pages > 5 && selectedPage < pages - 2) {
-      return (
-        <>
-          <div className="pagination-item" onClick={() => setSelectedPage(1)}>
-            1
-          </div>
-          <div className="pagination-item-dots">...</div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(selectedPage - 1)}
+  const columns = [
+    {
+      accessorKey: "image",
+      header: "",
+      enableColumnFilter: false,
+      enableColumnActions: false,
+      muiTableHeadCellProps: { sx: { color: "green" } },
+      Cell: ({ renderedCellValue }) => (
+        <img
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+          src={
+            renderedCellValue
+              ? `${process.env.REACT_APP_SERVER_URL}/streamers/${renderedCellValue}`
+              : default_image
+          }
+          alt="streamer"
+        />
+      ),
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+      muiTableHeadCellProps: { sx: { color: "green" } },
+      Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>,
+    },
+    {
+      accessorKey: "platform",
+      header: "Platform",
+      muiTableHeadCellProps: { sx: { color: "green" } },
+      enableColumnFilter: false,
+      enableColumnActions: false,
+      filterValue: (row) => row.platform,
+      Cell: ({ renderedCellValue }) => {
+        const image = setPlatformImage(renderedCellValue);
+        return (
+          <Box
+            alignItems="center"
+            justifyContent="center"
+            display="inline-flex"
           >
-            {selectedPage - 1}
-          </div>
-          <div className="pagination-item" style={selectedStyle(selectedPage)}>
-            {selectedPage}
-          </div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(selectedPage + 1)}
-          >
-            {selectedPage + 1}
-          </div>
-          <div className="pagination-item-dots">...</div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(pages)}
-          >
-            {pages}
-          </div>
-        </>
-      );
-    } else if (selectedPage > 3 && pages > 5 && selectedPage >= pages - 2) {
-      return (
-        <>
-          <div className="pagination-item" onClick={() => setSelectedPage(1)}>
-            1
-          </div>
-          <div className="pagination-item-dots">...</div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(pages - 3)}
-          >
-            {pages - 3}
-          </div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(pages - 2)}
-            style={selectedStyle(pages - 2)}
-          >
-            {pages - 2}
-          </div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(pages - 1)}
-            style={selectedStyle(pages - 1)}
-          >
-            {pages - 1}
-          </div>
-          <div
-            className="pagination-item"
-            onClick={() => setSelectedPage(pages)}
-            style={selectedStyle(pages)}
-          >
-            {pages}
-          </div>
-        </>
-      );
-    }
+            {image && (
+              <img
+                width={20}
+                src={image}
+                alt="pltform"
+                style={{ paddingRight: "10px" }}
+              />
+            )}
 
-    const pagination = [];
-    for (let i = 0; i < pages && i < 4; i++) {
-      pagination.push(
-        <div
-          className="pagination-item"
-          key={i}
-          onClick={() => setSelectedPage(i + 1)}
-          style={selectedStyle(i + 1)}
-        >
-          {i + 1}
-        </div>
-      );
-    }
-    if (pages > 5) {
-      pagination.push(
-        <>
-          <div className="pagination-item" key="dots">
-            ...
-          </div>
-          <div
-            className="pagination-item"
-            key={pages}
-            onClick={() => setSelectedPage(pages)}
-          >
-            {pages}
-          </div>
-        </>
-      );
-    }
-    return pagination;
-  };
-
-  const filterPlatforms = () => {
-    const newArray = data.filter((item) => {
-      const itemName = item.platform.toLowerCase();
-      const platformsFiltered = filteredPlatforms.filter((platform) => {
-        const platformName = platform.name.toLowerCase();
-        return itemName === platformName;
-      });
-      return platformsFiltered.length > 0;
-    });
-    setFilteredData(newArray);
-    setShowFilter(false);
-  };
-
-  const handleCheckbox = (platform) => {
-    const includes = filteredPlatforms.filter(
-      (filter) => filter.name === platform.name
-    );
-    if (includes.length > 0) {
-      const newArray = filteredPlatforms.filter(
-        (item) => item.name.toLowerCase() !== platform.name.toLowerCase()
-      );
-      setFilteredPlatforms(newArray);
-    } else {
-      setFilteredPlatforms([...filteredPlatforms, platform]);
-    }
-  };
-
-  function checkboxIcon(platform) {
-    const find = filteredPlatforms.filter(
-      (item) => item.name.toLowerCase() === platform.name.toLowerCase()
-    );
-    if (find.length > 0) {
-      return checkbox;
-    } else {
-      return empty_checkbox;
-    }
-  }
+            <strong>{renderedCellValue}</strong>
+          </Box>
+        );
+      },
+    },
+    {
+      accessorKey: "votes_up",
+      header: "Votes Up",
+      enableColumnFilter: false,
+      enableColumnActions: false,
+      muiTableHeadCellProps: { sx: { color: "green" } },
+      Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>,
+    },
+    {
+      accessorKey: "votes_down",
+      header: "Votes Down",
+      enableColumnFilter: false,
+      enableColumnActions: false,
+      muiTableHeadCellProps: { sx: { color: "green" } },
+      Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>,
+    },
+  ];
 
   return (
     <Card
@@ -339,169 +160,19 @@ const Table = ({ reload, setReload }) => {
       }}
     >
       <Box p={2}>
-        <div className="search">
-          <input
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => handleSearch(e)}
-            name="search"
-          />
-        </div>
-        <div className="table">
-          <div className="table-header">
-            <div
-              className="table-header-item"
-              onClick={() => changeFilter("name")}
-            >
-              <span>Name</span>
-              {sort.includes("name") && (
-                <img
-                  className="filter-arrow"
-                  height={30}
-                  width={30}
-                  src={sort === "name_asc" ? arrow_up : arrow_down}
-                  alt="filter-arrow"
-                />
-              )}
-            </div>
-            <div
-              className="table-header-item"
-              onClick={() => setShowFilter(true)}
-            >
-              <span>Platform</span>
-              <img
-                className="filter-arrow"
-                height={30}
-                width={30}
-                src={filter}
-                alt="filter-arrow"
-              />
-            </div>
-            {showFilter && (
-              <div className="platform-filter">
-                <div className="platform-filter-top">
-                  <span>Select:</span>
-                  <img
-                    src={close}
-                    alt="exit"
-                    height={20}
-                    width={20}
-                    onClick={() => setShowFilter(false)}
-                  />
-                </div>
-                {platforms.length > 0 &&
-                  platforms.map((platform) => (
-                    <div
-                      className="platform-filter-item"
-                      key={platform.id}
-                      onClick={() => handleCheckbox(platform)}
-                    >
-                      <img
-                        src={checkboxIcon(platform)}
-                        height={20}
-                        width={20}
-                        alt={`checked-${platform.name}`}
-                      />
-                      <img
-                        src={`${process.env.REACT_APP_SERVER_URL}/platforms/${platform.image}`}
-                        height={20}
-                        width={20}
-                        alt={`platform-${platform.name}`}
-                      />
-                      <span>{platform.name}</span>
-                    </div>
-                  ))}
-                <button onClick={() => filterPlatforms()}>Apply</button>
-              </div>
-            )}
-            <div
-              className="table-header-item"
-              onClick={() => changeFilter("votes")}
-            >
-              <span>Votes</span>
-              {sort.includes("votes") && (
-                <img
-                  className="filter-arrow"
-                  height={30}
-                  width={30}
-                  src={sort === "votes_asc" ? arrow_up : arrow_down}
-                  alt="filter-arrow"
-                />
-              )}
-            </div>
-          </div>
-          <div className="table-body">
-            {filteredData.length > 0 &&
-              filteredData
-                .filter(
-                  (item, index) =>
-                    index >= (selectedPage - 1) * 10 &&
-                    index < selectedPage * 10
-                )
-                .map((item) => {
-                  const votesUp = item.votes_up
-                    ? countVotes("up", item.votes_up, item.votes_down)
-                    : 0;
-                  const votesDown = item.votes_down
-                    ? countVotes("down", item.votes_up, item.votes_down)
-                    : 0;
-                  return (
-                    <div className="table-body-row" key={item.id}>
-                      <div
-                        className="table-body-item"
-                        onClick={() => navigate(`/streamer/${item.id}`)}
-                      >
-                        <div className="img-box">
-                          <img
-                            className="image"
-                            src={
-                              item.image
-                                ? `${process.env.REACT_APP_SERVER_URL}/streamers/${item.image}`
-                                : default_image
-                            }
-                            height={100}
-                            width={100}
-                            alt="xqc"
-                          />
-                          <span>{item.name}</span>
-                        </div>
-                      </div>
-                      <div className="table-body-item">
-                        <img
-                          src={setImage(item.platform)}
-                          alt={`${item.platform}-img`}
-                          height={25}
-                          width={25}
-                        />
-                      </div>
-                      <div className="table-body-item">
-                        <div className="votes-up-box">
-                          <img
-                            src={up_vote}
-                            alt="up-vote"
-                            height={30}
-                            width={30}
-                          />
-                          <span>{votesUp}%</span>
-                        </div>
-                        <div className="votes-down-box">
-                          <img
-                            src={down_vote}
-                            alt="down-vote"
-                            height={30}
-                            width={30}
-                          />
-                          <span>{votesDown}%</span>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-          </div>
-          {filteredData.length > 10 && (
-            <div className="pagination">{renderPagination()}</div>
-          )}
-        </div>
+        <MaterialReactTable
+          columns={columns}
+          data={filteredData}
+          muiTableBodyRowProps={({ row }) => ({
+            onClick: () => {
+              console.log(row);
+              navigate(`/streamer/${row.original.id}`);
+            },
+            sx: {
+              cursor: "pointer", //you might want to change the cursor too when adding an onClick
+            },
+          })}
+        />
       </Box>
     </Card>
   );
